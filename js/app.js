4e8c3ca9e5e1c4cc0e8aca819cc575593c417200
Jean-Marie Comets <jean.marie.comets@gmail.com>
2014-10-17 14:05:55 +0200

    Panzoom

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+// AngularJS app
 (function() {
   var app = angular.module('app', []);
 
@@ -73,4 +74,23 @@
       }
     };
   });
+})();
+
+// Panzoom
+(function() {
+  (function($) {
+    $(function() {
+      var $panzoom = $('#map').panzoom();
+      $panzoom.parent().on('mousewheel.focal', function(e) {
+        e.preventDefault();
+        var delta = e.delta || e.originalEvent.wheelDelta;
+        var zoomOut = delta ? delta < 0 : e.originalEvent.deltaY > 0;
+        $panzoom.panzoom('zoom', zoomOut, {
+          increment: 0.1,
+          animate: false,
+          focal: e
+        });
+      });
+    });
+  }) (jQuery);
 })();
